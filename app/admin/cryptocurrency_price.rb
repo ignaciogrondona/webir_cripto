0c3df34cb149f1e33c7aed19bfffdaa16303a1d9
Nacho Grondona <nachogrondona@gmail.com>
2018-10-29 19:20:44 -0300

    Add bid and ask prices to database

--- app/admin/cryptocurrency_price.rb
+++ app/admin/cryptocurrency_price.rb
@@ -1,21 +1,11 @@
 ActiveAdmin.register CryptocurrencyPrice do
-  permit_params :exchange, :value, :datetime
-
-  action_item only: :index do
-    link_to 'Reload', action: 'get_last_value'
-  end
-
-  collection_action :get_last_value do
-    RequestsServices.new.get_last_price('all')
-    redirect_to collection_path, notice: "Data reloaded successfully!"
-  end
-
   index do
     selectable_column
     id_column
     column :type
     column :exchange
-    column :value
+    column :avg_price
+    column :bid_price
     column :datetime
     actions
   end
