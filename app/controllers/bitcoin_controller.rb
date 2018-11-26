f41ec0f43e46e9d12570d987f27c3483f67276e0
fedealdunate <fedealduc94@gmail.com>
2018-11-26 00:33:44 -0300

    graficas con bid y ask prices

--- app/controllers/bitcoin_controller.rb
+++ app/controllers/bitcoin_controller.rb
@@ -2,14 +2,17 @@ class BitcoinController < ApplicationController
   def index
     @prices = []
     Exchange.all.each do |exchange|
-      line = { name: exchange.name, data: {} }
+      line_bid = { name: "#{exchange.name} bid_price", data: {} }
+      line_ask = { name: "#{exchange.name} ask_price", data: {} }
       exchange.bitcoin_prices.last(20).each do |coin|
-        line[:data][coin.datetime.change(sec:0)] = coin.bid_price
+        line_bid[:data][coin.datetime.change(sec:0)] = coin.bid_price if coin.bid_price
+        line_ask[:data][coin.datetime.change(sec:0)] = coin.ask_price if coin.ask_price
       end
-      @prices.push(line)
+      @prices.push(line_bid) unless line_bid[:data].empty?
+      @prices.push(line_ask) unless line_ask[:data].empty?
     end
-    @min = (@prices.map {|price| price[:data].values }.min.min * 0.998).truncate
-    @max = (@prices.map {|price| price[:data].values }.max.max * 1.002).ceil
+    @min = (@prices.map {|price| price[:data].values }.flatten.min * 0.998).truncate
+    @max = (@prices.map {|price| price[:data].values }.flatten.max * 1.002).ceil
     @prices
   end
 
